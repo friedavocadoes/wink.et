bd68e1f52e0e0bb4ffc6832fdf883812f6bf4cfa
gautham <desertncamel@gmail.com>
2024-04-22 22:53:27 +0530

    signup component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatComponent, LoginComponent],
+  imports: [RouterOutlet, ChatComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
